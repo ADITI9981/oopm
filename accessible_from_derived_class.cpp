099e8a57c425c8fc3b586d69c915d278d77a9b23
ADITI9981 <munmunyadav9981@gmail.com>
2024-09-03 11:50:24 +0530

    Add files via upload

---
+++ accessible_from_derived_class.cpp
@@ -0,0 +1,37 @@
+// #include<iostream>
+// using namespace std;
+// class B
+// {
+//     public:
+//     int i;
+//     private:
+//     int j;
+//     protected:
+//     int k;
+// };
+// class D:public B
+// {
+//     public:
+//     void get()
+//     {
+//         cout<<"enter i";
+//         cin>>i;
+//         cout<<"enter j";
+//         cin>>j;
+//         cout<<"enter k";
+//         cin>>k;
+//     }
+//     void show()
+//     {
+//         cout<<"value of i is "<<i<<endl;
+//         cout<<"value of j is "<<j<<endl;
+//         cout<<"value of k is "<<k<<endl;
+//     }
+// };
+// int main()
+// {
+//     D obj;
+//     obj.get();
+//     obj.show();
+//     return 0;
+// }
---
+++ accessible_from_outside_the_class.cpp
@@ -0,0 +1,23 @@
+// #include<iostream>
+// using namespace std;
+// class IT
+// {
+//     public:
+//     int i;
+//     private:
+//     int j;
+//     protected:
+//     int k;
+   
+// };
+// int main()
+// {
+//     IT obj;
+//     obj.i=2;
+//     obj.j=3;
+//     obj.k=4;
+//     cout<<"value of i is "<<obj.i<<endl;
+//     cout<<"value of j is "<<obj.j<<endl;
+//     cout<<"value of k is "<<obj.k<<endl;
+//     return 0;
+// }
